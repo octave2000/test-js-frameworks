0f8a0cabaaf05fc110fb423ca11773b97ca607f0
Octave <116314271+octave2000@users.noreply.github.com>
2026-07-13 11:26:22 +0200

    Remove localhost fetch call causing build error

--- vue/src/components/TheWelcome.vue
+++ vue/src/components/TheWelcome.vue
@@ -5,8 +5,6 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
-
-const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
 
 <template>
@@ -16,7 +14,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     </template>
     <template #heading>Documentation</template>
 
-    Vue’s
+    Vue's
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
     provides you with all information you need to get started.
   </WelcomeItem>
@@ -44,7 +42,7 @@ const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
     <br />
 
     More instructions are available in
-    <a href="javascript:void(0)" @click="openReadmeInEditor"><code>README.md</code></a
+    <a href="https://github.com/octave2000/test-js-frameworks/blob/main/vue/README.md" target="_blank" rel="noopener"><code>README.md</code></a
     >.
   </WelcomeItem>
 
